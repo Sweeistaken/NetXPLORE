c08739a987d08a7a0452ed67a4f5f7202be4d6b6
amica <amica@SWEE-MINIPC>
2023-02-19 19:35:46 -0800

    Add project files.

---
+++ NetXPLORE/Form1.cs
@@ -0,0 +1,15 @@
+namespace NetXPLORE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void toolStripButton1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
